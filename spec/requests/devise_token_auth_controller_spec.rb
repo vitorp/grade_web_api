50e6e011655aef1a18d6301e0ab73c9bb10a48f6
vitor pontes <vitormax2005@hotmail.com>
2018-07-21 17:39:43 -0300

    add devise_token_auth specs

---
+++ spec/requests/devise_token_auth_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+include ActionController::RespondWith
+
+RSpec.describe "Use authentication", type: :request do
+  let(:new_user_params) do
+    { email: "email@gmail.com",
+      password: "password",
+      name: "Vitor",
+      nickname: "Pontes"
+    }
+  end
+
+  let(:current_user) { User.create(email: 'email@gmail.com', name: 'Vitor', nickname: 'Pontes', password: 'password') }
+  let(:current_user_params) do
+    { email: current_user.email,
+      password: 'password',
+    }
+  end
+
+  let(:user_headers) { { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' } }
+
+
+
+  it 'creates user' do
+    post user_registration_path, params: new_user_params
+    expect(response).to have_http_status(200)
+  end
+
+  it 'logs in' do
+    login
+    expect(response).to have_http_status(200)
+  end
+
+  def login
+    post user_session_path, params: current_user_params.to_json, headers: user_headers
+  end
+end
